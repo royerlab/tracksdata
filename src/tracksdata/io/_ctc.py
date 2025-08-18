9df4205987eac4ee9402c6ac124536cca65f394e
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2025-08-18 14:33:23 -0700

    fixing shape

--- src/tracksdata/io/_ctc.py
+++ src/tracksdata/io/_ctc.py
@@ -64,7 +64,10 @@ def compressed_tracks_table(graph: BaseGraph) -> np.ndarray:
         if len(df) > 0:
             track_id[3] = df[DEFAULT_ATTR_KEYS.TRACK_ID].item()
 
-    out_array = np.atleast_2d(np.asarray(tracks_data, dtype=int))
+    if len(tracks_data) == 0:
+        return np.empty((0, 4), dtype=int)
+
+    out_array = np.asarray(tracks_data, dtype=int)
     out_array = out_array[np.argsort(out_array[:, 0])]
 
     return out_array
