be2411c395cd8ce6727a2327b3acc3e0aacefee6
Teun Huijben <teun.huijben@czbiohub.org>
2026-08-07 11:08:42 -0700

    merge two io/geff files into 1

--- src/tracksdata/io/__init__.py
+++ src/tracksdata/io/__init__.py
@@ -1,8 +1,11 @@
 """Input/output utilities for loading and saving tracking data in various formats."""
 
 from tracksdata.io._ctc import compressed_tracks_table, from_ctc, to_ctc
-from tracksdata.io._geff_dtypes import convert_geff_prop_dtype, geff_prop_dtype
-from tracksdata.io._geff_metadata import read_graph_metadata
+from tracksdata.io._geff import (
+    convert_geff_prop_dtype,
+    geff_prop_dtype,
+    read_graph_metadata,
+)
 
 __all__ = [
     "compressed_tracks_table",
